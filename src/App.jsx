dbbbc2e002fb611ab9b1ed99542e9db8c7ddf0b3
Shayan Ashraf <aksamshayan@gmail.com>
2025-09-17 14:45:19 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,108 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useState } from 'react';
+import "./index.css";
+
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import AnimatedBackground from "./components/Background";
+import Navbar from "./components/Navbar";
+import Portofolio from "./Pages/Portofolio";
+import ContactPage from "./Pages/Contact";
+import ProjectDetails from "./components/ProjectDetail";
+import WelcomeScreen from "./Pages/WelcomeScreen";
+import NotFoundPage from "./Pages/404";
+
+import { AnimatePresence } from 'framer-motion';
+
+const Footer = () => (
+  <footer>
+    <center>
+      <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
+      <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
+        © 2025{" "}
+        <a
+          href="https://www.linkedin.com/in/shayan-ashraf-314b191b2"
+          className="hover:underline"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Shayan Ashraf™
+        </a>
+        {" | "}
+        <a
+          href="https://github.com/Shayan48"
+          className="hover:underline"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+        {" | "}
+        <a
+          href="https://www.instagram.com/shayan_ashraf_07"
+          className="hover:underline"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Instagram
+        </a>
+        . All Rights Reserved.
+      </span>
+    </center>
+  </footer>
+);
+
+const LandingPage = ({ showWelcome, setShowWelcome }) => {
+  return (
+    <>
+      <AnimatePresence mode="wait">
+        {showWelcome && (
+          <WelcomeScreen onLoadingComplete={() => setShowWelcome(false)} />
+        )}
+      </AnimatePresence>
+
+      {!showWelcome && (
+        <>
+          <Navbar />
+          <AnimatedBackground />
+          <Home />
+          <About />
+          <Portofolio />
+          <ContactPage />
+          <Footer />
+        </>
+      )}
+    </>
+  );
+};
+
+const ProjectPageLayout = () => (
+  <>
+    <ProjectDetails />
+    <Footer />
+  </>
+);
+
+function App() {
+  const [showWelcome, setShowWelcome] = useState(true);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <LandingPage
+              showWelcome={showWelcome}
+              setShowWelcome={setShowWelcome}
+            />
+          }
+        />
+        <Route path="/project/:id" element={<ProjectPageLayout />} />
+        <Route path="*" element={<NotFoundPage />} /> {/* 404 fallback route */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
